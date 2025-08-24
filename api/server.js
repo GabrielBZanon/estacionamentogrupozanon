1c57a614bc826f4ddd12ff0cff9bb5db1bc113c9
GabrielBZanon <barbosa.zanon10@gmail.com>
2025-08-24 11:24:33 -0300

    implementação vercel

---
+++ api/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('../src/routes.js');
+
+const port = process.env.PORT || 3001;
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+
+app.listen(port, (req, res) => {
+    console.log('API respondendo em http://localhost:' + port)
+});
+
+// Habilitar CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
